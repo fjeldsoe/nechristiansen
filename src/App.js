b9bc70316198ef16858b394f2a9bcbb4f80775b4
Anders Fjeldsø Christiansen <afc@hesehus.dk>
2017-08-10 10:46:15 +0200

    few changes

--- src/App.js
+++ src/App.js
@@ -13,24 +13,12 @@ class App extends Component {
 		this.provider = new firebase.auth.FacebookAuthProvider()
 
 		firebase.auth().signInWithPopup(this.provider).then(function(result) {
-		  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-		  var token = result.credential.accessToken;
-		  // The signed-in user info.
-		  var user = result.user;
-		  console.log(user);
+		  console.log(result);
 		}).catch(function(error) {
-		  // Handle Errors here.
-		  var errorCode = error.code;
-		  var errorMessage = error.message;
-		  // The email of the user's account used.
-		  var email = error.email;
-		  // The firebase.auth.AuthCredential type that was used.
-		  var credential = error.credential;
-		  // ...
+		  console.log(error);
 		});
 
-		this.storage = firebase.storage()
-		this.storageRef = this.storage.ref()
+		this.storageRef = firebase.storage().ref()
 
 		this.drop_handler = this.drop_handler.bind(this)
 		this.dragover_handler = this.dragover_handler.bind(this)
@@ -47,11 +35,11 @@ class App extends Component {
 			for (var i=0; i < dt.items.length; i++) {
 				if (dt.items[i].kind === "file") {
 					var f = dt.items[i].getAsFile();
-					const imagesRef = this.storageRef.child(f.name);
-					imagesRef.put(f).then(function(snapshot) {
+					console.log(f.name)
+					const uploadTask = this.storageRef.child('/images/' + f.name).put(f)
+					uploadTask.then(function(snapshot) {
 					  console.log('Uploaded a blob or file!');
 					});
-					console.log("... file[" + i + "].name = " + f.name);
 				}
 			}
 		} else {
--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const config = {
         authDomain: "nechristiansen-7ad6d.firebaseapp.com",
         databaseURL: "https://nechristiansen-7ad6d.firebaseio.com",
         projectId: "nechristiansen-7ad6d",
-        storageBucket: "default",
+        storageBucket: "nechristiansen-7ad6d.appspot.com",
         messagingSenderId: "627280494573"
     }
 }
